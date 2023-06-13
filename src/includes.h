3e6e8cc50859947f23d6332ba9ad8b7723590e50
SChaotyx <AgustinAngelCAB@gmail.com>
2023-06-13 03:39:46 -0300

    Setup

---
+++ src/includes.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <gd.h>
+#include <iostream>
+#include <matdash.hpp>
+#include <matdash/minhook.hpp>
+
+using namespace cocos2d;
+using namespace gd;
+
+using std::uintptr_t;
